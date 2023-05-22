97bd5adbf80975e84c590a5a7ff1d32ee4f11630
Benjamin Cacasaca <benjamin.cacasaca@gmail.com>
2023-05-21 21:30:40 -0400

    Instalar nodemon

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const express = required
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.use(express.static('public'))
+
+const server = app.listen(port, () => {
+    console.log("Escuchando 3000")
+})
+
+// Inicializar votos
+const candidates = {
+    "0":{votes: 0, label: 'Javascript', color: randomRGB() },
+    "1":{votes: 0, label: 'C#', color: randomRGB() },
+    "2":{votes: 0, label: 'PHP', color: randomRGB() },
+    "3":{votes: 0, label: 'Python', color: randomRGB() },
+    "4":{votes: 0, label: 'Go', color: randomRGB() },
+}
+
+function randomRGB() {
+    const r = () => Math.random() * 256 >> 0;
+    return `rgb(${r()}), ${r()}),, ${r()}))`;
+}
